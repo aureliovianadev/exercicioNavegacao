93608bde66ebf573d019d87134143f3b096c780d
aureliovianadev <aureliovianadev@gmail.com>
2025-10-31 20:33:54 -0300

    Calculadora introduzida

--- screens/calculadoraScreen.js
+++ screens/calculadoraScreen.js
@@ -1,9 +1,76 @@
-import {View, Text, StyleSheet} from 'react-native'
+import { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 
-export default function CalculadoraScreen(){
-    return(
+export default function CalculadoraScreen() {
+
+    const [num1, setNum1] = useState('')
+    const [num2, setNum2] = useState('')
+    const [resultado, setResultado] = useState('')
+
+    function somar() {
+        setResultado(parseInt(num1) + parseInt(num2))
+    }
+    function subtrair() {
+        setResultado(parseInt(num1) - parseInt(num2))
+    }
+    function multiplicar() {
+        setResultado(parseInt(num1) * parseInt(num2))
+    }
+    function dividir() {
+        setResultado(parseInt(num1) / parseInt(num2))
+    }
+    
+    return (
         <View>
             <Text>Calculadora</Text>
+
+            <TextInput
+                placeholder="Digite um n°"
+                value={num1}
+                onChangeText={setNum1}
+            />
+
+            <TextInput
+                placeholder="Digite um n°"
+                value={num2}
+                onChangeText={setNum2}
+            />
+
+            <View style={estilos.botao}>
+                <Button
+                    title="Somar"
+                    onPress={() => somar()}
+                />
+            </View>
+
+            <View style={estilos.botao}>
+                <Button
+                    title="Subtrair"
+                    onPress={() => subtrair()}
+                />
+            </View>
+
+            <View style={estilos.botao}>
+                <Button
+                    title="Multiplicar"
+                    onPress={() => multiplicar()}
+                />
+            </View>
+
+            <View style={estilos.botao}>
+                <Button
+                    title="Dividir"
+                    onPress={() => dividir()}
+                />
+            </View>
+            <Text>Resultado: {resultado}</Text>
+
         </View>
-    ) 
+    )
 }
+
+const estilos = StyleSheet.create({
+    botao: {
+        marginVertical: 10
+    }
+})
--- screens/imcScreen.js
+++ screens/imcScreen.js
@@ -1,9 +1,55 @@
-import {View, Text, StyleSheet} from 'react-native'
+import { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+
 
 export default function ImcScreen(){
+
+    const [massa, setMassa] = useState('')
+    const [altura, setAltura] = useState('')
+    const [imc, setImc] = useState('')
+
+    function IMC() {
+        setImc( parseInt(massa) / ( parseFloat(altura) * parseFloat(altura)))
+    }
+    
     return(
         <View>
             <Text>IMC</Text>
-        </View>
+
+            <TextInput
+                placeholder="Digite o seu peso"
+                value={massa1}
+                onChangeText={setMassa}
+            />
+
+            <TextInput
+                placeholder="Digite sua altura em metros"
+                value={altura}
+                onChangeText={setAltura}
+            />
+
+        <View style={estilos.botao}>
+        <Button
+          title="IMC"
+          onPress={() => imc()}
+          />
+      </View>
+
+       <Text>Resultado: {imc} 
+
+        Menor que 18,5	Magreza	0
+        Entre 18,5 e 24,9	Normal	0
+        Entre 25,0 e 29,9	Sobrepeso	I
+        Entre 30,0 e 39,9	Obesidade	II
+        Maior que 40,0	Obesidade Grave	III
+       </Text>
+       
+        </View>    
     ) 
 }
+
+const estilos = StyleSheet.create({
+  botao: {
+    marginVertical: 10
+  }
+})
